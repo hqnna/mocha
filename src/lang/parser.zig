dd3a52c67ee92b8aa24c6f9ad266ce924b25844f
hanna <hanna@hanna.lol>
2023-03-13 14:26:54 -0400

    feat: implement the ability to parse arrays

--- src/lang/parser.zig
+++ src/lang/parser.zig
@@ -9,12 +9,13 @@ allocator: std.mem.Allocator,
 
 const RuleSet = ptk.RuleSet(tkn.Token);
 const Core = ptk.ParserCore(tkn.Tokenizer, .{.space});
+const Error = Core.Error || std.mem.Allocator.Error || std.fmt.ParseFloatError;
 
 pub fn init(allocator: std.mem.Allocator, t: *tkn.Tokenizer) Parser {
     return Parser{ .core = Core.init(t), .allocator = allocator };
 }
 
-fn acceptIdentifier(p: *Parser) ![]const u8 {
+fn acceptIdentifier(p: *Parser) Error![]const u8 {
     const state = p.core.saveState();
     errdefer p.core.restoreState(state);
 
@@ -29,18 +30,19 @@ test "identifier parsing" {
     try std.testing.expectEqualStrings(data, try p.acceptIdentifier());
 }
 
-fn acceptValue(p: *Parser) !types.Value {
+fn acceptValue(p: *Parser) Error!types.Value {
     const state = p.core.saveState();
     errdefer p.core.restoreState(state);
 
     const t = try p.core.accept(RuleSet.oneOf(.{
-        .boolean, .number, .string, .nil,
+        .boolean, .number, .string, .nil, .array_start,
     }));
 
     return switch (t.type) {
         .number => types.Value{ .number = try std.fmt.parseFloat(f64, t.text) },
         .boolean => types.Value{ .boolean = std.mem.eql(u8, t.text, "true") },
         .string => types.Value{ .string = t.text[1 .. t.text.len - 1] },
+        .array_start => try p.acceptArray(),
         .nil => types.Value.nil,
         else => unreachable,
     };
@@ -58,7 +60,7 @@ test "value parsing" {
     try std.testing.expectEqual(types.Value.nil, try p.acceptValue());
 }
 
-fn acceptField(p: *Parser) !types.Field {
+fn acceptField(p: *Parser) Error!types.Field {
     const state = p.core.saveState();
     errdefer p.core.restoreState(state);
 
@@ -76,4 +78,33 @@ test "field parsing" {
 
     try std.testing.expectEqualStrings("hello", field.name);
     try std.testing.expectEqual(true, field.value.boolean);
+}
+
+fn acceptArray(p: *Parser) Error!types.Value {
+    const state = p.core.saveState();
+    errdefer p.core.restoreState(state);
+
+    var items = std.ArrayList(types.Value).init(p.allocator);
+    defer items.deinit();
+
+    while (try p.core.peek()) |next| switch (next.type) {
+        else => try items.append(try p.acceptValue()),
+        .array_end => break,
+    };
+
+    _ = try p.core.accept(RuleSet.is(.array_end));
+
+    return types.Value{ .array = .{
+        .items = try items.toOwnedSlice(),
+    } };
+}
+
+test "array parsing" {
+    var t = tkn.Tokenizer.init("['hello' 'world']", null);
+    var p = init(std.testing.allocator, &t);
+    const value = try p.acceptValue();
+
+    try std.testing.expectEqualStrings("hello", value.array.items[0].string);
+    try std.testing.expectEqualStrings("world", value.array.items[1].string);
+    value.array.deinit(std.testing.allocator);
 }
--- src/lang/types.zig
+++ src/lang/types.zig
@@ -1,6 +1,9 @@
+const Allocator = @import("std").mem.Allocator;
+
 pub const Value = union(enum) {
     string: []const u8,
     boolean: bool,
+    array: Array,
     number: f64,
     nil: void,
 };
@@ -8,4 +11,17 @@ pub const Value = union(enum) {
 pub const Field = struct {
     name: []const u8,
     value: Value,
+};
+
+pub const Array = struct {
+    items: []Value,
+
+    pub fn deinit(array: Array, allocator: Allocator) void {
+        for (array.items) |item| switch (item) {
+            .array => |v| v.deinit(allocator),
+            else => continue,
+        };
+
+        allocator.free(array.items);
+    }
 };
