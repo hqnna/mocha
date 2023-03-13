7aa7cb479c9e1cf0f251b5a576fef7eeab665619
hanna <hanna@hanna.lol>
2023-03-13 16:43:55 -0400

    test: remove redundant test in main file

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,5 @@ const std = @import("std");
 const Parser = @import("lang/parser.zig");
 
 comptime {
-    std.testing.refAllDeclsRecursive(@This());
-}
-
-test "parse a complete example" {
-    const data = try std.fs.cwd().readFileAlloc(
-        std.testing.allocator,
-        "examples/rke.mocha",
-        std.math.maxInt(usize),
-    );
-
-    var document = try Parser.parse(std.testing.allocator, data);
-    defer document.deinit(std.testing.allocator);
-    defer std.testing.allocator.free(data);
+    std.testing.refAllDeclsRecursive(Parser);
 }
