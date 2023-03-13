0b7058e1c30baba163b30376c609446d81ee0804
hanna <hanna@hanna.lol>
2023-03-13 15:02:16 -0400

    test: test a complete example mocha file

--- src/main.zig
+++ src/main.zig
@@ -6,3 +6,17 @@ comptime {
     std.testing.refAllDeclsRecursive(tokenizer);
     std.testing.refAllDeclsRecursive(Parser);
 }
+
+test "parse a complete example" {
+    const data = try std.fs.cwd().readFileAlloc(
+        std.testing.allocator,
+        "examples/rke.mocha",
+        std.math.maxInt(usize),
+    );
+
+    var fields = try Parser.parse(std.testing.allocator, data);
+    fields[6].value.object.deinit(std.testing.allocator);
+    fields[5].value.array.deinit(std.testing.allocator);
+    std.testing.allocator.free(fields);
+    std.testing.allocator.free(data);
+}
