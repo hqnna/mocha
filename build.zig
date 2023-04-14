46224843746ca26d498895991cfb204fb8231893
hanna <hanna@hanna.lol>
2023-04-14 20:37:44 +0000

    build: update to latest build system changes

--- build.zig
+++ build.zig
@@ -1,24 +1,25 @@
 const std = @import("std");
 
-const source = std.Build.FileSource{
-    .path = "src/main.zig",
-};
-
 pub fn build(b: *std.Build) anyerror!void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const ptk = b.dependency("ptk", .{});
 
-    const mocha = b.addModule("mocha", .{ .source_file = source });
-    try mocha.dependencies.put("ptk", ptk.module("parser-toolkit"));
+    _ = b.addModule("mocha", .{
+        .source_file = .{ .path = "src/main.zig" },
+        .dependencies = &.{std.Build.ModuleDependency{
+            .module = ptk.module("parser-toolkit"),
+            .name = "ptk",
+        }},
+    });
 
     const tests = b.addTest(.{
-        .root_source_file = source,
+        .root_source_file = .{ .path = "src/main.zig" },
         .optimize = optimize,
         .target = target,
     });
 
     const test_step = b.step("test", "Run library tests");
     tests.addModule("ptk", ptk.module("parser-toolkit"));
-    test_step.dependOn(&tests.run().step);
+    test_step.dependOn(&b.addRunArtifact(tests).step);
 }
